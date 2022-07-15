edfd09b82e77e7da148d911dcaf5c69e75442202
TruongAnim <truonganim.dev@gmail.com>
2022-07-15 22:34:08 +0700

    update bubble sort

--- wbw/_07_2022/transformations/test_transform.py
+++ wbw/_07_2022/transformations/test_transform.py
@@ -3,11 +3,21 @@ from manim import *
 config.set_width = "80%"
 
 
-SCENE_NAME = "T1"
+SCENE_NAME = "TestSwap"
 
 if __name__ == "__main__":
     print(__file__)
     os.system(f"manim -pql {__file__} {SCENE_NAME}")
+
+class TestTransform(Scene):
+    def construct(self):
+        circle = Circle().rotate(PI/2)
+        square = Square()
+        self.play(Create(circle))
+        circle.rotate(-PI/4)
+
+        self.play(Transform(circle, square))
+        self.wait()
 
 class T1(Scene):
 
@@ -56,12 +66,21 @@ class T1(Scene):
         self.play(Uncreate(tex))
         self.wait()
 
-class TestTransform(Scene):
-    def construct(self):
-        circle = Circle().rotate(PI/2)
-        square = Square()
-        self.play(Create(circle))
-        circle.rotate(-PI/4)
+class TestSwap(Scene):
+    def setup(self):
+        self.A = Square()
+        self.B = Circle()
+        self.G = VGroup(self.A,self.B).arrange(RIGHT, buff=LARGE_BUFF*2)
+        self.add(self.G)
 
-        self.play(Transform(circle, square))
+    def construct(self):
+        self.play(*[
+            Transform(self.A, self.A.copy().move_to(self.B.get_center())),
+            Transform(self.B, self.B.copy().move_to(self.A.get_center())),
+
+        ],run_time=2)
+        self.wait()
+        self.G[0], self.G[1] = self.G[1], self.G[0]
+        self.play(self.G[0].animate.scale(2))
+        self.play(self.G[1].animate.scale(0.5))
         self.wait()
