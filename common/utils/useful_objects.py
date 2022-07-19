3ef1f718caaf540244647514627ddba05c76f80f
TruongAnim <truonganim.dev@gmail.com>
2022-07-19 22:30:23 +0700

    edit numberline test

--- common/utils/useful_objects.py
+++ common/utils/useful_objects.py
@@ -12,3 +12,4 @@ class TestNumberLine(Scene):
     def setup(self):
         line1 = NumberLine(x_range=[0, 10, 1])
         self.add(line1.set_stroke(width=1))
+        self.add(MathTex(r"f(x)={x}^{2}").next_to(line1, DOWN))
