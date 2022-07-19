2d625295b819f80fdce46413fa83f86bbaea1d9e
TruongAnim <truonganim.dev@gmail.com>
2022-07-19 21:52:41 +0700

    add numberline test

---
+++ common/utils/useful_objects.py
@@ -0,0 +1,14 @@
+from manim import *
+
+SCENE_NAME = "TestNumberLine"
+
+if __name__ == "__main__":
+    command = f"manim -pql {__file__} {SCENE_NAME}"
+    print(command)
+    os.system(command)
+
+
+class TestNumberLine(Scene):
+    def setup(self):
+        line1 = NumberLine(x_range=[0, 10, 1])
+        self.add(line1.set_stroke(width=1))
