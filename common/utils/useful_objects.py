b89380ad734c0d0c456157c59613174d6573bd46
TruongAnim <truonganim.dev@gmail.com>
2022-07-20 22:09:16 +0700

    add ex3 useful mobject

--- common/utils/useful_objects.py
+++ common/utils/useful_objects.py
@@ -1,6 +1,6 @@
 from manim import *
 
-SCENE_NAME = "TestNumberLine"
+SCENE_NAME = "TestTable"
 
 if __name__ == "__main__":
     command = f"manim -pql {__file__} {SCENE_NAME}"
@@ -13,3 +13,16 @@ class TestNumberLine(Scene):
         line1 = NumberLine(x_range=[0, 10, 1])
         self.add(line1.set_stroke(width=1))
         self.add(MathTex(r"f(x)={x}^{2}").next_to(line1, DOWN))
+
+
+class TestTable(Scene):
+    def setup(self):
+        table = Table(
+            [list(map(str,[1,2,3])),
+             list(map(str,[4,5,6]))],
+            col_labels=[Text(f"Col {i+1}") for i in range(3)],
+            row_labels=[Text(f"Row {i+1}") for i in range(2)],
+            include_outer_lines=True
+        )
+        self.add(table)
+        cell = table.get_cell([1,2])
